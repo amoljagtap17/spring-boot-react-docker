fc6b86c89d939037bc30047ddb730d53494ba1b9
Amol Jagtap <amoljagtap17@gmail.com>
2019-07-26 22:45:26 +0530

    SPRING-BOOT-REACT-DOCKER: Add Antd Table Component

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,12 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import { Table } from 'antd'
+import axios from './axios'
+
+const columns = [
+  {
+    title: 'Student Id',
+    dataIndex: 'studentId',
+    key: 'studentId'
+  },
+  {
+    title: 'First Name',
+    dataIndex: 'firstName',
+    key: 'firstName'
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'lastName',
+    key: 'lastName'
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email'
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: 'gender'
+  }
+]
 
 function App() {
   const [students, setStudents] = useState([])
 
   useEffect(() => {
     const getAllStudents = async () => {
-      const res = await axios.get('/students')
+      const res = await axios.get('students')
 
       setStudents(res.data)
     }
@@ -15,15 +44,7 @@ function App() {
   }, [])
 
   if (students.length > 0) {
-    return students.map(({ studentId, firstName, lastName, gender, email }) => (
-      <div key={studentId}>
-        <h2>{studentId}</h2>
-        <p>{firstName}</p>
-        <p>{lastName}</p>
-        <p>{gender}</p>
-        <p>{email}</p>
-      </div>
-    ))
+    return <Table dataSource={students} columns={columns} rowKey="studentId" />
   }
 
   return <h1>No Student Found!!</h1>
