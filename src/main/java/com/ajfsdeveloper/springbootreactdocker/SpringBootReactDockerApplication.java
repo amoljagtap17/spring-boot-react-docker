6134ecc9807044440051aa316909ccd09b6fdcb8
Amol Jagtap <amoljagtap17@gmail.com>
2019-07-26 20:33:19 +0530

    SPRING-BOOT-REACT-DOCKER: Spring Initialzer project from start.spring.io

---
+++ src/main/java/com/ajfsdeveloper/springbootreactdocker/SpringBootReactDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ajfsdeveloper.springbootreactdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactDockerApplication.class, args);
+	}
+
+}
