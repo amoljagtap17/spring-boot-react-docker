c50054daa6e27976fdf69357a248fafe55b804c1
Amol Jagtap <amoljagtap17@gmail.com>
2019-07-26 22:16:35 +0530

    SPRING-BOOT-REACT-DOCKER: Fetch Student data using axios and display in React app

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  return <h1>React App with Spring Boot!</h1>
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    const getAllStudents = async () => {
+      const res = await axios.get('/students')
+
+      setStudents(res.data)
+    }
+
+    getAllStudents()
+  }, [])
+
+  return (
+    <Fragment>
+      <h1>React App with Spring Boot!</h1>
+      <ul>
+        {students.map(student => (
+          <li key={student.studentId}>
+            {student.firstName} - {student.lastName}
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  )
 }
 
 export default App
