bb2c8b1b6d9a47f62019430061cd48b132b4c3de
Amol Jagtap <amoljagtap17@gmail.com>
2019-07-28 01:26:32 +0530

    SPRING-BOOT-REACT-DOCKER: Add Footer component

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Table, Spin, Icon } from 'antd'
 import { columns } from './columns'
 import Container from './Container'
+import Footer from './Footer'
 import axios from './axios'
 
 const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />
@@ -41,6 +42,7 @@ function App() {
           />
         )}
         {students.length === 0 && <h1>No Student Found!!</h1>}
+        <Footer numberOfStudents={students.length} />
       </Container>
     )
   }
---
+++ src/js/src/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Container from './Container'
+import { Button, Avatar } from 'antd'
+import './footer.css'
+
+const Footer = props => (
+  <div className="footer">
+    <Container>
+      {props.numberOfStudents ? (
+        <Avatar
+          style={{ backgroundColor: '#f56a00', marginRight: '5px' }}
+          size="large"
+        >
+          {props.numberOfStudents}
+        </Avatar>
+      ) : null}
+      <Button type="primary">Add New Student +</Button>
+    </Container>
+  </div>
+)
+
+export default Footer
