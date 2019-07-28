7cf5c732ba2b1bb2f1e774490f2b7d2e4d1d9a46
Amol Jagtap <amoljagtap17@gmail.com>
2019-07-28 12:23:58 +0530

    SPRING-BOOT-REACT-DOCKER: Add POST Mapping on server

--- src/main/java/com/ajfsdeveloper/springbootreactdocker/controllers/StudentController.java
+++ src/main/java/com/ajfsdeveloper/springbootreactdocker/controllers/StudentController.java
@@ -3,9 +3,7 @@ package com.ajfsdeveloper.springbootreactdocker.controllers;
 import com.ajfsdeveloper.springbootreactdocker.models.Student;
 import com.ajfsdeveloper.springbootreactdocker.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,10 @@ public class StudentController {
     @GetMapping
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addNewStudent(@RequestBody Student student) {
+        System.out.println(student);
     }
 }
